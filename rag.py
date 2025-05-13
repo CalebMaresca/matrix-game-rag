f16141d079c6e672b2aa716bc09a62ba9e7b96c3
Caleb Maresca <marescacc@gmail.com>
2025-05-13 10:53:27 -0400

    edit vector search tool to allow injecting context into state

--- rag.py
+++ rag.py
@@ -2,11 +2,13 @@ from langgraph.graph import END, StateGraph
 from langchain_core.vectorstores import VectorStore
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.language_models.chat_models import BaseChatModel
-from langchain_core.tools import tool
+from langchain_core.tools import tool, InjectedToolCallId
+from langchain_core.messages import ToolMessage
 from typing import Callable, List, Annotated
 from langchain_core.documents import Document
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
+from langgraph.types import Command
 
 class RagState(TypedDict):
   messages: Annotated[list, add_messages]
@@ -59,12 +61,32 @@ def make_rag_graph(model: BaseChatModel, vector_store: VectorStore, template: Ch
 
   return rag_graph.compile()
 
-def create_vector_search_tool(vector_store: VectorStore, search_kwargs: dict) -> Callable:
-  @tool("vector-search")
-  def vector_search_tool(query: str) -> List[str]:
-    """Searches a vector database for the given query and returns relevant document contents."""
-    retriever = vector_store.as_retriever(search_kwargs=search_kwargs)
-    retrieved_docs = retriever.invoke(query)
-    return [doc.page_content for doc in retrieved_docs]
-  return vector_search_tool
+def create_vector_search_tool(vector_store: VectorStore, search_kwargs: dict, inject_to_state: bool = False) -> Callable:
+  # WARNING: the graph state REQUIRES a 'context' key if inject_to_state is True
+  if inject_to_state:
+    @tool("vector-search")
+    def vector_search_tool(query: str, tool_call_id: Annotated[str, InjectedToolCallId]) -> Command:
+      """Searches a vector database for the given query and returns relevant document contents."""
+      retriever = vector_store.as_retriever(search_kwargs=search_kwargs)
+      retrieved_docs = retriever.invoke(query)
+      return Command(update={
+          "context": [doc.page_content for doc in retrieved_docs],
+          # update the message history
+          "messages": [
+              ToolMessage(
+                  f"{[doc.page_content for doc in retrieved_docs]}",
+                  tool_call_id=tool_call_id
+              )
+          ]
+      })
+    return vector_search_tool
+  
+  else:
+    @tool("vector-search")
+    def vector_search_tool(query: str) -> List[str]:
+      """Searches a vector database for the given query and returns relevant document contents."""
+      retriever = vector_store.as_retriever(search_kwargs=search_kwargs)
+      retrieved_docs = retriever.invoke(query)
+      return [doc.page_content for doc in retrieved_docs]
+    return vector_search_tool
 
